876366fd8d1deb9352d759da569312ba671e7c5b
Asya <a.gurova.1@stud.elka.pw.edu.pl>
2020-01-16 01:04:16 +0100

    Wykres usredniony, koncowe wartosci filtrow

--- Proba.py
+++ Proba.py
@@ -6,8 +6,10 @@ from psd import *
 import matplotlib.pyplot as plt
 from biosppy.signals import ecg
 import numpy as np
+import matplotlib
 
-# Sample frequency
+
+
 Fs = 1000 # Hz
 def notchFitler(data, Fs, frequency, Q):
     nyquistFrequency = Fs/2.
@@ -25,55 +27,32 @@ def highFilter(data, Fs, lowFrequency ):
     normalizedCutoff = lowFrequency / nyquistFrequency
     b, a = signal.butter(5, normalizedCutoff, btype='high')
     return filtfilt(b, a, data)
-def bandpassFilter(data, Fs, lowf, highf):
-    nyquistFrequency = Fs / 2.
-    normalizedCutoffH = highf / nyquistFrequency
-    normalizedCutoffL = lowf / nyquistFrequency
-    b,a = signal.butter(5, [normalizedCutoffL, normalizedCutoffH], btype='band')
-    return lfilter(b, a, data)
-def allWithOne (signal, lowf, highf, Fs, Q, notch1, notch2):
-    y1 = highFilter(signal, Fs, lowf)
-    y1 = lowFilter(y1, Fs, highf)
-    y1 = notchFitler(y1, Fs, notch1, Q)
-    y1 = notchFitler(y1, Fs, notch2, Q)
-    return y1
-def hpException (signal, highf, Fs, Q, notch1, notch2):
-    y1 = lowFilter(signal, Fs, highf)
-    y1 = notchFitler(y1, Fs, notch1, Q)
-    y1 = notchFitler(y1, Fs, notch2, Q)
-    return y1
 def peaksAndAveraged (signal, visibility):
     out = ecg.ecg(signal, sampling_rate= Fs, show = visibility)
     rpeaks = out['rpeaks']
     templates = out['templates']
     average_final = np.average(templates, axis=0)
     return rpeaks, average_final, templates
-def powerSpectral (signal):
-    freqs, psd1 = powerSpectralDensity(signal, Fs)
-    #Plot spectrum
-    plt.loglog(freqs, psd1)
-    plt.xlabel("Czestotliwość (Hz)")
-    plt.ylabel("Amplituda (pT)")
-    plt.grid()
-    plt.show()
-def plotAvereged (templates, average_final):
-    ts_tmpl = np.linspace(0, 0.6, templates.shape[1], endpoint=False)
-    plt.plot(ts_tmpl, average_final)
-    plt.xlabel("Czas (s)")
-    plt.ylabel("Amplituda (pT)")
-    plt.grid()
-    plt.show()
-
+def move_figure(f, x, y):
+    """Move figure's upper left corner to pixel (x, y)"""
+    backend = matplotlib.get_backend()
+    if backend == 'TkAgg':
+        f.canvas.manager.window.wm_geometry("+%d+%d" % (x, y))
+    elif backend == 'WXAgg':
+        f.canvas.manager.window.SetPosition((x, y))
+    else:
+        # This works for QT and GTK
+        # You can also use window.setGeometry
+        f.canvas.manager.window.move(x, y)
 
 #File loading
 filename = "Tom II MCG.tsv"
 #filename = "janmasny_MKG3.txt"
-#filename = "janmasny_MKG2.txt"
 #filename = "szumy (1).txt"
 
 data = loadtxt(filename)
-y1 = data[:,1]
-y2 = data[:,3]*0.5
+data1 = data[:,1]*2
+data2 = data[:,3]*0.9
 
 # Clip out good data
 
@@ -92,17 +71,35 @@ y2 = data[:,3]*0.5
 #y2= y2[64000:73500]
 
 #wiekszy zakres
-y1 = y1[5500:80000]
-y2 = y2[5500:80000]
+yw1 = data1[5500:80000]
+yw2 = data2[5500:80000]
+
+
+#Filters
+yw1= highFilter (yw1, Fs, lowFrequency=2)
+yw2 = highFilter (yw2, Fs, lowFrequency=1)
+
+yw1 = notchFitler(yw1, Fs, 50, 1)
+yw1 = notchFitler(yw1, Fs, 100, 50)
+
+yw2 = notchFitler(yw2, Fs, 50, 1)
+yw2 = notchFitler(yw2, Fs, 60, 20)
+yw2 = notchFitler(yw2, Fs, 100, 50)
+
+yw1 = lowFilter(yw1, Fs, 150)
+yw2 = lowFilter(yw2, Fs, 150)
+
+'''yw1 = lowFilter(yw1, Fs, 45)
+yw2 = lowFilter(yw2, Fs, 45)'''
+
+'''y1=yw1[5500:15500]
+y2=yw2[5500:15500]'''
+y1=yw1
+y2=yw2
 
 times = arange(len(y1))/Fs
 
-# HP filters
-'''y1 = highFilter (y1, Fs, lowFrequency=1)
-y2 = highFilter (y2, Fs, lowFrequency=0.3)'''
-y1 = allWithOne (y1, lowf = 1, highf=100, Fs=Fs, Q=50, notch1=50, notch2=100)
-y2 = allWithOne (y2, lowf = 0.3, highf=100, Fs=Fs, Q=50, notch1=50, notch2=100)
-#Plot timeseries for avereged signal
+'''#Plot timeseries for avereged signal
 ave2sensors=(y1+y2)/2                                           #Averaging signals
 #powerSpectral (ave2sensors)
 rpeaks, average_final, templates = peaksAndAveraged (ave2sensors, False)
@@ -114,41 +111,62 @@ plt.xlabel("Czas (s)")
 plt.ylabel("Amplituda (pT)")
 plt.grid()
 plt.show()
-#pl1 = plotAvereged (templates, average_final)                         #Averaged signal
+#pl1 = plotAvereged (templates, average_final)                         #Averaged signal'''
 
 #Plot timeseries for both signals
-#powerSpectral (y1)
-#powerSpectral (y2)
+
 rpeaks1, average_final1, templates1 = peaksAndAveraged (y1, False)
 rpeaks2, average_final2, templates2 = peaksAndAveraged (y2, False)
+ts_tmpl = np.linspace(0, 0.6, templates1.shape[1], endpoint=False)
 
-fig, axs = plt.subplots(2)
-fig.suptitle('Sygnały z dwóch czujników')
-axs[0].plot(times, y1, 'tab:orange')
-axs[0].set(xlabel='Czas (s)', ylabel='Amplituda (pT)')
-axs[0].scatter(rpeaks1/1000, y1[rpeaks1], c='red')
-axs[0].grid()
-axs[1].set(xlabel='Czas (s)', ylabel='Amplituda (pT)')
-axs[1].plot(times, y2, 'tab:red')
-axs[1].scatter(rpeaks2/1000, y2[rpeaks2], c='red')
-axs[1].grid()
+#Ploty
+
+
+fig, axs = plt.subplots(1,2, figsize = (19,7))
+move_figure(fig, 0, 0)
+#fig.suptitle('Power Spectral Density')
+freqs, psd1 = powerSpectralDensity(yw1, Fs)
+axs[0].semilogy(freqs, psd1, 'tab:orange')
+freqs, psd2 = powerSpectralDensity(yw2, Fs)
+axs[1].semilogy(freqs, psd2)
+for ax in axs.flat:
+    ax.set(xlabel='Częstotliwość (pT)', ylabel='Amplituda (pT)')
+    ax.set_xscale('log')
+    ax.grid()
 plt.show()
 
-#Averaged signal
-#pl2 = plotAvereged (templates1, average_final1)
-#pl3 = plotAvereged (templates2, average_final2)
+fig, axs = plt.subplots(2, figsize = (19,7))
+move_figure(fig, 0, 0)
+#fig.suptitle('Signals from 2 sensors')
+axs[0].plot(times, y1, 'tab:orange')
+axs[0].scatter(rpeaks1/1000, y1[rpeaks1])
+axs[1].plot(times, y2)
+axs[1].scatter(rpeaks2/1000, y2[rpeaks2], c='orange')
+for ax in axs.flat:
+    ax.set(xlabel='Czas (s)', ylabel='Amplituda (pT)')
+    ax.grid()
+plt.show()
 
-fig, axs = plt.subplots(3)
-ts_tmpl = np.linspace(0, 0.6, templates.shape[1], endpoint=False)
-axs[0].plot(ts_tmpl, average_final1)
-axs[0].grid()
-#axs[0].set_title ('Pierwszy czyjnik')
-axs[1].plot(ts_tmpl, average_final2)
-axs[1].grid()
-#axs[1].set_title ('Drugi czyjnik')
-axs[2].plot(ts_tmpl, average_final)
-axs[2].grid()
-#axs[2].set_title ('Uśredniony z obu czujników')
+
+fig, axs = plt.subplots(1,2, figsize = (19,7))
+move_figure(fig, 0, 0)
+#fig.suptitle('Nałożone sygnały')
+for beat in range(0,templates1.shape[0]):
+    axs[0].plot(ts_tmpl, templates1[beat,:], 'tab:orange')
+for beat in range(0, templates2.shape[0]):
+    axs[1].plot(ts_tmpl, templates2[beat,:], '#1f77b4')
 for ax in axs.flat:
     ax.set(xlabel='Czas (ms)', ylabel='Amplituda (pT)')
+    ax.grid()
+plt.show()
+
+
+fig, axs = plt.subplots(1,2, figsize = (19,7))
+move_figure(fig, 0, 0)
+#fig.suptitle('Averaged signals')
+axs[0].plot(ts_tmpl, average_final1, 'tab:orange')
+axs[1].plot(ts_tmpl, average_final2)
+for ax in axs.flat:
+    ax.set(xlabel='Czas (ms)', ylabel='Amplituda (pT)')
+    ax.grid()
 plt.show()
